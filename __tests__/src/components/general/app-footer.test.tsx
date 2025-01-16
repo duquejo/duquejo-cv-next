68c65a2fa1bc4de529cf682c03e27c1163485dde
José Duque <duquejo01@gmail.com>
2025-01-16 12:54:41 -0500

    :white_check_mark: feat(tests): app-footer & theme-toggle tests

--- __tests__/src/components/general/app-footer.test.tsx
+++ __tests__/src/components/general/app-footer.test.tsx
@@ -2,25 +2,25 @@ import { render, type RenderResult, screen } from '@testing-library/react';
 import { AppFooter, SOCIAL_DATA as social } from '@/components/general/app-footer';
 
 describe('<AppFooter /> tests', () => {
-  let result: RenderResult;
+  let component: RenderResult;
 
   beforeEach(() => {
-    result = render(<AppFooter />);
+    component = render(<AppFooter />);
 
-    const copyDate = result.container.querySelector('span');
+    const copyDate = component.container.querySelector('span');
     if (copyDate) {
       copyDate.textContent = '© 1992 José Duque';
     }
   });
 
-  it('Should be rendered & match the snapshot', () => {
+  it('Should match the snapshot', () => {
     // Arrange & Act
-    const footerElement = screen.getByRole('contentinfo');
+    const footer = screen.getByRole('contentinfo');
 
     // Assert
-    expect(result.container).toMatchSnapshot();
-    expect(footerElement).toBeInTheDocument();
-    expect(footerElement).toHaveTextContent('© 1992 José Duque');
+    expect(component.container).toMatchSnapshot();
+    expect(footer).toBeInTheDocument();
+    expect(footer).toHaveTextContent('© 1992 José Duque');
 
     social.map((s) => {
       const link = screen.getByRole('link', { name: s.name });
---
+++ __tests__/src/components/general/theme-toggle.test.tsx
@@ -0,0 +1,147 @@
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { ThemeToggle } from '@/components/general/theme-toggle';
+import userEvent from '@testing-library/user-event';
+import { useTheme, type UseThemeProps } from 'next-themes';
+
+vi.mock('next-themes', () => ({
+  useTheme: vi.fn(),
+}));
+
+describe('<ThemeToggle /> tests', () => {
+  const setThemeMock = vi.fn();
+
+  const mockThemeArgs: UseThemeProps = {
+    theme: 'light',
+    themes: ['light', 'dark', 'system'],
+    systemTheme: 'light',
+    setTheme: setThemeMock,
+  };
+
+  beforeEach(() => {
+    vi.clearAllMocks();
+    vi.mocked(useTheme).mockReturnValue({ ...mockThemeArgs });
+  });
+
+  it('Should match the snapshot with the default args', () => {
+    const { container } = render(<ThemeToggle />);
+
+    const button = screen.getByRole('button');
+
+    expect(container).toMatchSnapshot();
+
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveAttribute('data-state', 'closed');
+    expect(button).toHaveAttribute('aria-expanded', 'false');
+    expect(button).toHaveAttribute('title', 'Toggle theme');
+    expect(button).toHaveTextContent('Toggle theme');
+    expect(button).toHaveClass('text-primary-foreground', 'hover:bg-primary/90');
+  });
+
+  it('Should match the snapshot with a given args', async () => {
+    const { container } = render(
+      <ThemeToggle variant="secondary" title="Testing toggle" className="bg-color-red" />,
+    );
+
+    const button = screen.getByRole('button');
+    const btnSvgs = container.querySelectorAll('svg');
+
+    expect(container).toMatchSnapshot();
+
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveAttribute('title', 'Testing toggle');
+    expect(button).toHaveTextContent('Testing toggle');
+    expect(button).toHaveClass(
+      'text-secondary-foreground',
+      'hover:bg-secondary/80',
+      'bg-color-red',
+    );
+
+    expect(btnSvgs).toHaveLength(2);
+  });
+
+  it('Should handle the system theme by default', async () => {
+    vi.mocked(useTheme).mockReturnValue({
+      ...mockThemeArgs,
+      theme: 'system',
+      systemTheme: 'dark',
+    });
+
+    render(<ThemeToggle variant="secondary" />);
+
+    const selectedTheme = screen.getByLabelText('Dark mode');
+    expect(selectedTheme).toHaveAttribute('aria-selected', 'true');
+
+    await userEvent.click(screen.getByRole('button'));
+
+    await waitFor(() => {
+      const selectedThemeOption = screen.getByText('System');
+      expect(selectedThemeOption).toHaveClass('bg-accent');
+
+      fireEvent.click(selectedThemeOption);
+    });
+
+    expect(setThemeMock).toHaveBeenCalledWith('system');
+  });
+
+  it('Should handle the light theme', async () => {
+    vi.mocked(useTheme).mockReturnValue({ ...mockThemeArgs });
+
+    render(<ThemeToggle />);
+
+    const selectedTheme = screen.getByLabelText('Light mode');
+    expect(selectedTheme).toHaveAttribute('aria-selected', 'true');
+
+    await userEvent.click(screen.getByRole('button'));
+
+    await waitFor(() => {
+      const selectedThemeOption = screen.getByText('Light');
+      expect(selectedThemeOption).toHaveClass('bg-accent');
+
+      fireEvent.click(selectedThemeOption);
+    });
+
+    expect(setThemeMock).toHaveBeenCalledWith('light');
+  });
+
+  it('Should handle the dark theme', async () => {
+    vi.mocked(useTheme).mockReturnValue({
+      ...mockThemeArgs,
+      theme: 'dark',
+      systemTheme: 'dark',
+    });
+
+    render(<ThemeToggle />);
+
+    const selectedTheme = screen.getByLabelText('Dark mode');
+    expect(selectedTheme).toHaveAttribute('aria-selected', 'true');
+
+    await userEvent.click(screen.getByRole('button'));
+
+    await waitFor(() => {
+      const selectedThemeOption = screen.getByText('Dark');
+      expect(selectedThemeOption).toHaveClass('bg-accent');
+
+      fireEvent.click(selectedThemeOption);
+    });
+
+    expect(setThemeMock).toHaveBeenCalledWith('dark');
+  });
+
+  it('Should trigger the button click events', async () => {
+    render(<ThemeToggle variant="secondary" title="Testing toggle" className="bg-color-red" />);
+
+    const button = screen.getByRole('button');
+    const buttonWrapper = button.parentElement!;
+
+    await userEvent.click(button);
+
+    expect(button).toHaveAttribute('data-state', 'open');
+    expect(button).toHaveAttribute('aria-expanded', 'true');
+    expect(buttonWrapper).toHaveAttribute('aria-hidden', 'true');
+    expect(buttonWrapper).toHaveAttribute('data-aria-hidden', 'true');
+
+    const themeOptionsDropdown = await waitFor(() => screen.getAllByRole('menuitem'));
+
+    expect(themeOptionsDropdown).toHaveLength(3);
+  });
+});
